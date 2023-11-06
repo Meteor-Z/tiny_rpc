9d805d48194791f5a6ebf79fd22e7c2fec28e2b7
lzc <3030218083@qq.com>
2023-11-06 19:09:02 +0800

    update

--- src/include/net/time/timer.h
+++ src/include/net/time/timer.h
@@ -19,8 +19,9 @@ public:
     ~Timer();
     void add_time_event(std::shared_ptr<TimerEvent> event);
     void delete_time_event(std::shared_ptr<TimerEvent> event);
+    
     // 发生IO事件之后，需要执行的方法
-    void on_time();
+    void on_timer();
 
 private:
     void reset_arrive_time();
@@ -31,11 +32,12 @@ private:
     // epoll_event m_listen_events {};                      // epoll事件
     // std::function<void()> m_read_callback { nullptr };   // 读回掉函数
     // std::function<void()> m_write_callback { nullptr };  // 写回掉函数
+
     // std::multimap<key(arrive_time), TimerEvent> m_pending_events;
     std::multimap<int64_t, std::shared_ptr<TimerEvent>>
         m_pending_events; // time_event的集合
 
-    std::mutex m_mtx;
+    std::mutex m_mtx; // 锁
 };
 } // namespace rpc
 #endif
--- src/net/time/timer.cc
+++ src/net/time/timer.cc
@@ -19,26 +19,28 @@
 namespace rpc {
 Timer::~Timer() {}
 Timer::Timer() : FdEvent() {
+    // 定时器
     m_fd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
     rpc::utils::DEBUG_LOG(fmt::format("timer fd = {}", m_fd));
 
-    // fdevent 在eventloop上进行监听 绑定一个可读事件
-    listen(FdEvent::TriggerEvent::IN_EVENT, std::bind(&Timer::on_time, this));
+    // fdevent 在eventloop上进行监听 绑定一个可读事件 listen 是继承与FdEvent得到的
+    listen(FdEvent::TriggerEvent::IN_EVENT, std::bind(&Timer::on_timer, this));
 }
 
-void Timer::on_time() {
+void Timer::on_timer() {
     char buf[8];
     // 处理缓冲区事件
     while (true) {
-        if ((read(m_fd, buf, 8) == -1) && errno == EAGAIN)
+        if ((read(m_fd, buf, 8) == -1) && errno == EAGAIN) {
             break;
+        }
     }
 
     int64_t now_time = rpc::utils::get_now_ms(); // 当前的事件
 
     std::vector<std::shared_ptr<TimerEvent>> temp;
     std::vector<std::pair<int64_t, std::function<void()>>> tasks;
-    std::unique_lock<std::mutex> lock{m_mtx};
+    std::unique_lock<std::mutex> lock { m_mtx };
     auto it = m_pending_events.begin();
 
     for (; it != m_pending_events.end(); it++) {
@@ -75,7 +77,7 @@ void Timer::on_time() {
 }
 
 void Timer::reset_arrive_time() {
-    std::unique_lock<std::mutex> lock{m_mtx};
+    std::unique_lock<std::mutex> lock { m_mtx };
     auto temp = m_pending_events;
     lock.unlock();
     if (temp.empty()) {
@@ -84,7 +86,7 @@ void Timer::reset_arrive_time() {
 
     int64_t now = rpc::utils::get_now_ms();
     auto it = temp.begin();
-    int64_t inteval{0};
+    int64_t inteval { 0 };
     if (it->second->get_arrive_time() > now) {
         inteval = it->second->get_arrive_time() - now;
     } else {
@@ -102,18 +104,18 @@ void Timer::reset_arrive_time() {
 
     int rt = timerfd_settime(m_fd, 0, &value, nullptr);
     if (rt != 0) {
-        rpc::utils::ERROR_LOG(
-            fmt::format("timerfd_settime error, errno = {} error = {}", errno,
-                        strerror(errno)));
+        rpc::utils::ERROR_LOG(fmt::format("timerfd_settime error, errno = {} error = {}",
+                                          errno, strerror(errno)));
     }
 
     rpc::utils::DEBUG_LOG(fmt::format("timer reset to {}", now + inteval));
 }
 
 void Timer::add_time_event(std::shared_ptr<TimerEvent> event) {
-    std::unique_lock<std::mutex> unique_lock{m_mtx}; // 需要加锁
+    // 需要加锁
+    std::unique_lock<std::mutex> unique_lock { m_mtx };
     // 需不需重新设置超时事件
-    bool is_reset_timerfd{false};
+    bool is_reset_timerfd { false };
     if (m_pending_events.empty()) {
         is_reset_timerfd = true;
     } else {
@@ -133,7 +135,7 @@ void Timer::add_time_event(std::shared_ptr<TimerEvent> event) {
 
 void Timer::delete_time_event(std::shared_ptr<TimerEvent> event) {
     event->set_cancel(true);
-    std::unique_lock<std::mutex> unique_lock{m_mtx};
+    std::unique_lock<std::mutex> unique_lock { m_mtx };
     auto begin = m_pending_events.lower_bound(event->get_arrive_time());
     auto end = m_pending_events.upper_bound(event->get_arrive_time());
     auto it = begin;
@@ -146,7 +148,7 @@ void Timer::delete_time_event(std::shared_ptr<TimerEvent> event) {
         m_pending_events.erase(it);
     }
 
-    rpc::utils::DEBUG_LOG(fmt::format("success delete time_event at arrive {}",
-                                      event->get_arrive_time()));
+    rpc::utils::DEBUG_LOG(
+        fmt::format("success delete time_event at arrive {}", event->get_arrive_time()));
 }
 } // namespace rpc
