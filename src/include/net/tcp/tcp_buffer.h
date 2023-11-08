9c95486a6e81742bf29623a424db82403846aff6
lzc <3030218083@qq.com>
2023-11-08 09:04:26 +0800

    update commnet

--- src/include/net/tcp/tcp_buffer.h
+++ src/include/net/tcp/tcp_buffer.h
@@ -13,6 +13,7 @@ TcpBuffer
 #ifndef RPC_NET_TCP_BUFFER_H
 #define RPC_NET_TCP_BUFFER_H
 
+#include <chrono>
 #include <string>
 #include <vector>
 
@@ -21,24 +22,48 @@ class TcpBuffer {
 public:
     TcpBuffer(int size);
     ~TcpBuffer();
-    int can_read_bytes_num();  // 可写字节数
-    int can_write_bytes_num(); // 可写的字节数
-    int read_index();
-    int wtite_index();
-    void read_to_buffer(const char* buffer, int size);
+
+    // 可以读的字节数
+    int can_read_bytes_num() const noexcept;
+
+    // 可写的字节数
+    int can_write_bytes_num() const noexcept;
+
+    int read_index() const noexcept;
+
+    int wtite_index() const noexcept;
+
+    // 写入到 buffer 里面
+    // buffer: 字节数
+    // size: buffer的大小
+    void write_to_buffer(const char* buffer, int size);
+
+    // 读出字节
+    // re: 读入到此 std::vector<char> 里面
+    // size: 大小
     void read_from_buffer(std::vector<char>& re, int size);
+
+    // 调整，将buffer变成最初始的状态
     void adjust_buffer();
+    
+    // 扩容
     void resize_buffer(int new_size);
+
+    // 往右边调整， read_index 从新坐标开始
     void adjust_read_index(int size);
+    
+    // 往右边调整，将 write_indx 从新坐标开始
     void adjust_write_index(int size);
 
+    std::vector<char>& get_buffer() noexcept;
+
 private:
-    int m_read_index { 0 }; // 读取的位置
-    int m_write_index { 0 }; // 写入的位置是
-    int m_size { 0 };
+    int m_read_index { 0 };  // 读取的位置
+    int m_write_index { 0 }; // 写入的位置
+    int m_size { 0 };        // ？
 
 public:
-    std::vector<char> m_buffer {};
+    std::vector<char> m_buffer {}; // 读入字节
 };
 }; // namespace rpc
 
--- src/net/tcp/tcp_buffer.cc
+++ src/net/tcp/tcp_buffer.cc
@@ -1,91 +1,109 @@
-#include "net/tcp/tcp_buffer.h"
-#include "common/log.h"
-
-#include <fmt/format.h>
-
 #include <cstring>
 #include <memory_resource>
 #include <vector>
-
+#include <fmt/core.h>
+#include "net/tcp/tcp_buffer.h"
+#include "common/log.h"
 
 namespace rpc {
-    
-    TcpBuffer::TcpBuffer(int size) { m_buffer.resize(size); }
 
-    TcpBuffer:: ~TcpBuffer() { }
+TcpBuffer::TcpBuffer(int size) : m_size(size) { m_buffer.resize(size); }
 
-    int TcpBuffer:: can_read_bytes_num() { return m_write_index - m_read_index; }
+TcpBuffer::~TcpBuffer() {}
 
-    int TcpBuffer::can_write_bytes_num() { return m_buffer.size() - m_write_index; }
-
-    int TcpBuffer::read_index() { return m_read_index; }
-
-    int TcpBuffer::wtite_index() { return m_write_index; }
-
-    void TcpBuffer::read_to_buffer(const char* buffer, int size) {
-        if (size > can_write_bytes_num()) {
-            int new_size = { static_cast<int>(1.5 * (m_write_index + size)) };    
-            resize_buffer(new_size);
-        }
-       std::memcpy(&m_buffer[m_write_index], buffer, size);
-    }
-
-    void TcpBuffer::resize_buffer(int new_size) {
-        std::vector<char> new_buffer(new_size);
-        int count = std::min(new_size, can_read_bytes_num());
-        std::memcpy(&new_buffer[0], &m_buffer[m_read_index], count);
-        m_buffer.swap(new_buffer);
-
-        m_read_index = 0;
-        m_write_index = m_read_index + count;
-        adjust_buffer();
-    }
-
-    void TcpBuffer::read_from_buffer(std::vector<char>& re, int size) {
-        if (can_read_bytes_num() == 0) { return; }
-        
-        int read_size  { std::min(size, can_read_bytes_num()) };
-        
-        std::vector<char> temp(read_size);
-        std::memcpy(&temp[0], &m_buffer[m_read_index], read_size);
-        re.swap(temp);
-        m_read_index +=read_size;
-    }
-
-    void TcpBuffer::adjust_read_index(int size) {
-        int j = m_read_index + size;
-        if (j >= m_buffer.size()) {
-            rpc::utils::ERROR_LOG(fmt::format("ajust_read_index() error, invalid size {}, old_read_index {}, buffer size{}",
-            size, m_read_index, m_buffer.size()));
-            return;
-        }
-        m_read_index = j;
-        adjust_buffer();
-        
-    }
-
-    void TcpBuffer::adjust_write_index(int size) {
-       int j = m_write_index + size;
-        if (j >= m_buffer.size()) {
-            rpc::utils::ERROR_LOG(fmt::format("ajust_read_index() error, invalid size {}, old_read_index {}, buffer size{}",
-            size, m_read_index, m_buffer.size()));
-            return;
-        }
-        m_write_index = j;
-        adjust_buffer();
-    }
-
-    void TcpBuffer::adjust_buffer() {
-        if (m_read_index < static_cast<int>(m_buffer.size() / 3)) {
-            return;
-        }
-        
-        std::vector<char> new_buffer(m_buffer.size());
-
-        int count { can_read_bytes_num() };
-        std::memcpy(&new_buffer[0], &m_buffer[m_read_index], count);
-        m_buffer.swap(new_buffer);
-        m_read_index = 0;
-        m_write_index = m_read_index + count;
-    }
+int TcpBuffer::can_read_bytes_num() const noexcept {
+    return m_write_index - m_read_index;
 }
+
+int TcpBuffer::can_write_bytes_num() const noexcept {
+    return m_buffer.size() - m_write_index;
+}
+
+int TcpBuffer::read_index() const noexcept { return m_read_index; }
+
+int TcpBuffer::wtite_index() const noexcept { return m_write_index; }
+
+void TcpBuffer::write_to_buffer(const char* buffer, int size) {
+    if (size > can_write_bytes_num()) {
+        int new_size = { static_cast<int>(1.5 * (m_write_index + size)) };
+        resize_buffer(new_size);
+    }
+
+    // 调整
+    std::memcpy(&m_buffer[m_write_index], buffer, size);
+}
+
+void TcpBuffer::resize_buffer(int new_size) {
+    std::vector<char> new_buffer(new_size);
+
+    int count = std::min(new_size, can_read_bytes_num());
+    
+    // 进行拷贝
+    std::memcpy(&new_buffer[0], &m_buffer[m_read_index], count);
+    
+    m_buffer.swap(new_buffer);
+
+    m_read_index = 0;
+    m_write_index = m_read_index + count;
+    adjust_buffer();
+}
+
+void TcpBuffer::read_from_buffer(std::vector<char>& re, int size) {
+    // 没有可以读入的字节
+    if (can_read_bytes_num() == 0) {
+        return;
+    }
+    
+    // 可以读入的字节
+    int read_size { std::min(size, can_read_bytes_num()) };
+
+    std::vector<char> temp(read_size);
+    std::memcpy(&temp[0], &m_buffer[m_read_index], read_size);
+    re.swap(temp);
+
+    // 可以读入的下标要往右边移动，移动的距离就是 read_size
+    m_read_index += read_size;
+}
+
+void TcpBuffer::adjust_read_index(int size) {
+    size_t j = m_read_index + size;
+    if (j >= m_buffer.size()) {
+        rpc::utils::ERROR_LOG(fmt::format(
+            "ajust_read_index() error, invalid size {}, old_read_index {}, buffer size{}",
+            size, m_read_index, m_buffer.size()));
+        return;
+    }
+    m_read_index = j;
+    adjust_buffer();
+}
+
+void TcpBuffer::adjust_write_index(int size) {
+    int j = m_write_index + size;
+    if (j >= m_buffer.size()) {
+        rpc::utils::ERROR_LOG(fmt::format(
+            "ajust_read_index() error, invalid size {}, old_read_index {}, buffer size{}",
+            size, m_read_index, m_buffer.size()));
+        return;
+    }
+    m_write_index = j;
+    adjust_buffer();
+}
+
+void TcpBuffer::adjust_buffer() {
+    // 读的字节数 > 整个字节的 1/3, 才会进行调整
+    if (m_read_index < static_cast<int>(m_buffer.size() / 3)) {
+        return;
+    }
+
+    std::vector<char> new_buffer(m_buffer.size());
+
+    int count { can_read_bytes_num() };
+    std::memcpy(&new_buffer[0], &m_buffer[m_read_index], count);
+    m_buffer.swap(new_buffer);
+    m_read_index = 0;
+    m_write_index = m_read_index + count;
+
+    // new_buffer.clear();
+}
+std::vector<char>& TcpBuffer::get_buffer() noexcept { return m_buffer; }
+} // namespace rpc
