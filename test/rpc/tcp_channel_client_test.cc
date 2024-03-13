7a79f5b0044dd77e69212fd89fd37331cbe547b0
lzc <3030218083@qq.com>
2024-03-13 16:24:41 +0800

    feat: delete other header file

--- test/rpc/tcp_channel_client_test.cc
+++ test/rpc/tcp_channel_client_test.cc
@@ -3,18 +3,10 @@
 #include "net/rpc/rpc_channel.h"
 #include "net/rpc/rpc_closure.h"
 #include "net/rpc/rpc_controller.h"
-#include "net/rpc/rpc_dispatchor.h"
-#include "net/tcp/tcp_server.h"
 #include "order.pb.h" // 导入的proto生成的文件
-#include "google/protobuf/service.h"
 #include "common/log.h"
-#include "net/coder/protobuf_protocol.h"
-#include "net/coder/abstract_protocol.h"
 #include "net/tcp/ipv4_net_addr.h"
-#include "net/tcp/tcp_client.h"
-#include "net/tcp/tcp_connection.h"
 #include "common/log_config.h"
-#include "net/coder/protobuf_protocol.h"
 
 // void test_tcp_dispatchor_client() {
 //     std::shared_ptr<rpc::IPv4NetAddr> addr =
